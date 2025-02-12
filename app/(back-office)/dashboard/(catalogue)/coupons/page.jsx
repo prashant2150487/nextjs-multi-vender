b773900baceb45c24c487e393a5fc8d0f7c232f5
Your Name <your-email@example.com>
2025-02-12 23:40:37 +0530

    add code

--- app/(back-office)/dashboard/(catalogue)/coupons/page.jsx
+++ app/(back-office)/dashboard/(catalogue)/coupons/page.jsx
@@ -1,7 +1,18 @@
 import React from "react";
+import { PageHeader } from "../../../../../components/backoffice/PageHeader";
+import TableActions from "../../../../../components/backoffice/TableActions";
 
 const Coupons = () => {
-  return <div>Coupan</div>;
+  return (
+    <>
+      <PageHeader
+        heading="Coupons"
+        href="/dashboard/coupons/new"
+        linkTitle="Add Coupons"
+      />
+      {/* <TableActions /> */}
+    </>
+  );
 };
 
 export default Coupons;
